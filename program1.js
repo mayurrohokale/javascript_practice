be9365b43df9fd807bc5b3af7e50a86a40110a84
mayurrohokale <mayurrohokale12345@gmail.com>
2024-02-20 22:30:54 +0530

    variables to store data

--- program1.js
+++ program1.js
@@ -1,2 +1,4 @@
 let message = "First Program";
+
+message = "message changed!";
 console.log(message);
---
+++ program2.js
@@ -0,0 +1,8 @@
+const orange_color = "#F03";
+const red_color = "#F009";
+
+let color = orange_color;
+
+console.log(color);
+
+
