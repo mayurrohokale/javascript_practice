2202768b6a114f8b43fd45ec1e702b6c4a7c5740
mayurrohokale <mayurrohokale12345@gmail.com>
2024-02-20 23:06:56 +0530

    data types and functions

---
+++ program4.js
@@ -0,0 +1,26 @@
+let Num = "Marvel";
+console.log(typeof(Num));
+
+Num[0]= "s";
+console.log(Num);
+console.log(Num[0]);
+console.log(Num.length);
+
+let check = true;
+console.log(typeof(check));
+
+let  arr1 = [2,56,6,-2, 0];
+arr1.sort();
+console.log(arr1);
+
+
+function hello()
+{ 
+    let str = "Hello";
+    let name = "Mayur";
+
+    console.log(str + name);
+
+}
+
+hello();
