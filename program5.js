a0fefc4d21439ec461f5541e66a2c0c7584c3f06
mayurrohokale <mayurrohokale12345@gmail.com>
2024-02-21 23:38:03 +0530

    hoisting in javascript

---
+++ program5.js
@@ -0,0 +1,10 @@
+let a = hgti;
+let b =20;
+
+function add(a,b)
+{
+    let sum = String(a) + b;
+    console.log(sum);
+}
+
+add(a,b);
---
+++ program6.js
@@ -0,0 +1,10 @@
+getName();
+console.log(x);
+
+let x =20;
+
+function getName()
+{
+    console.log("Jay Ganesh....");
+}
+
