f5faa6f9f3838ba0c4d3d0d7f53218d7af6f4f8d
mayurrohokale <mayurrohokale12345@gmail.com>
2024-02-27 07:52:14 +0530

    defined and undefined

---
+++ program9.js
@@ -0,0 +1,16 @@
+console.log(a);
+var a = 10;
+console.log(a);
+
+var x;
+
+console.log(a);
+
+if(x === undefined)
+{
+    console.log("x is undefined");
+}
+else 
+{
+    console.log("x is defined");
+}
