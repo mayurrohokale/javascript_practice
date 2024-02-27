6c21e29f466bd0840bf9a4430d91f7bdb00938d5
mayurrohokale <mayurrohokale12345@gmail.com>
2024-02-27 07:01:47 +0530

    Hoisting in javascript

---
+++ program7.js
@@ -0,0 +1,19 @@
+console.log(x);
+x = 10
+var x;
+console.log(x);
+a();
+b();
+
+
+function a()
+{
+    var x = 20;
+    console.log(x);
+}
+
+function b()
+{
+    var x = 30;
+    console.log(x);
+}
