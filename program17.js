da9f0b82fd84cba8b4b972930bf3a8ba880a6661
mayurrohokale <mayurrohokale12345@gmail.com>
2024-10-15 10:20:54 +0530

    Prefix and postfix

---
+++ program17.js
@@ -0,0 +1,13 @@
+// prefix and postfix
+
+let x = 3;
+ y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
