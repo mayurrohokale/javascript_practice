144792972b15642cc6a0636cb5d0c693a38b98a2
mayurrohokale <mayurrohokale12345@gmail.com>
2024-10-17 11:05:12 +0530

    Date and Time

--- program23.js
+++ program23.js
@@ -2,7 +2,7 @@
 
 
 const score = [1, 5, 8]
-const hundred = 1223.459
+const hundred = 1223.501
 
 console.log(Math.ceil(hundred));
 
@@ -11,4 +11,8 @@ console.log(Math.ceil(hundred));
 console.log(Math.random());
 console.log(Math.max(...score));
 console.log(Math.min(...score));
+console.log(Math.round(hundred));
+
+console.log(Math.floor(1000+  Math.random()*9000))
+
 
---
+++ program24.js
@@ -0,0 +1,25 @@
+// Date and Time
+
+const now = new Date();
+
+console.log(now.toLocaleDateString());
+console.log(now.getTimezoneOffset());
+console.log(typeof now);
+
+
+
+const myCreatedDate = new Date(2021, 9 ,25)
+
+console.log(myCreatedDate.toDateString());
+console.log(myCreatedDate.toLocaleDateString());
+console.log(myCreatedDate.getDay());
+
+let myTimeStamps = new Date.now()
+
+console.log(myTimeStamps);
+
+
+
+
+
+
